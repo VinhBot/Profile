cb19f0385ecb8941779b46e1389d7d0e635b9347
VinhBot <115027531+VinhBot@users.noreply.github.com>
2023-09-16 06:38:36 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components/dist/styled-components.esm.js";  
+
+const Main = styled.div({
+  "text-align": "center",
+  "vertical-align": "center",
+  "line-height": "100vh"
+});
+
+export default function App() {
+  return (
+    <Main className="hieuungnhapnhay">
+      Khà khà. chả có gì đâu mà xem :))
+    </Main>
+  );
+};
